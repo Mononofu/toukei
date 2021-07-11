1b59fc0e32b686cdd0d128e523fc7fe2efb29613
Julian Schrittwieser <j.schrittwieser@gmail.com>
2021-07-11 15:52:53 +0100

    log query details

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,20 @@ extern crate rocket;
 
 use rocket::http::hyper::header;
 use rocket::http::ContentType;
-use rocket::request::Request;
+use rocket::request::{FromRequest, Outcome, Request};
 use rocket::response::{self, Responder, Response};
+use rocket::State;
+use std::net::SocketAddr;
 
 #[get("/")]
 fn index() -> &'static str {
     "Please see https://github.com/Mononofu/toukei"
 }
 
-const EMPTY_PNG: &[u8] = include_bytes!("1x1.png");
+// A responder that returns an empty 1x1.png and disallows caching.
 struct PixelResponder {}
+
+const EMPTY_PNG: &[u8] = include_bytes!("1x1.png");
 
 impl<'r> Responder<'r, 'static> for PixelResponder {
     fn respond_to(self, _: &'r Request<'_>) -> response::Result<'static> {
@@ -24,19 +28,81 @@ impl<'r> Responder<'r, 'static> for PixelResponder {
     }
 }
 
-#[get("/visit.png?<url>")]
-fn visit(url: &str) -> PixelResponder {
-    println!("got url {}", url);
+#[derive(Debug)]
+struct UserAgent(Option<String>);
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for UserAgent {
+    type Error = std::convert::Infallible;
+
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        Outcome::Success(UserAgent(
+            req.headers().get_one("user-agent").map(|s| s.to_owned()),
+        ))
+    }
+}
+
+#[get("/visit.png?<url>&<referrer>&<bot>")]
+fn visit_pixel(
+    url: &str,
+    referrer: Option<&str>,
+    bot: Option<&str>,
+    ua: UserAgent,
+    addr: SocketAddr,
+    ip_reader: &State<maxminddb::Reader<Vec<u8>>>,
+) -> PixelResponder {
+    handle_visit(url, referrer, bot, ua, addr, ip_reader);
     PixelResponder {}
 }
 
-#[post("/exit.png?<url>")]
-fn exit(url: &str) -> PixelResponder {
-    println!("exiting url {}", url);
+#[post("/visit?<url>&<referrer>&<bot>")]
+fn visit_beacon(
+    url: &str,
+    referrer: Option<&str>,
+    bot: Option<&str>,
+    ua: UserAgent,
+    addr: SocketAddr,
+    ip_reader: &State<maxminddb::Reader<Vec<u8>>>,
+) -> PixelResponder {
+    handle_visit(url, referrer, bot, ua, addr, ip_reader);
     PixelResponder {}
+}
+
+#[post("/exit?<url>&<referrer>&<bot>")]
+fn exit_beacon(
+    url: &str,
+    referrer: Option<&str>,
+    bot: Option<&str>,
+    ua: UserAgent,
+    addr: SocketAddr,
+    ip_reader: &State<maxminddb::Reader<Vec<u8>>>,
+) -> PixelResponder {
+    handle_visit(url, referrer, bot, ua, addr, ip_reader);
+    PixelResponder {}
+}
+
+fn handle_visit(
+    url: &str,
+    referrer: Option<&str>,
+    bot: Option<&str>,
+    ua: UserAgent,
+    addr: SocketAddr,
+    ip_reader: &State<maxminddb::Reader<Vec<u8>>>,
+) {
+    let city: Option<maxminddb::geoip2::City> = ip_reader.lookup(addr.ip()).unwrap_or(None);
+    println!(
+        "got url {} from {} ({:?}) {:?}, coming from {:?} - bot {:?}",
+        url, addr, city, ua, referrer, bot
+    );
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, visit, exit])
+    let reader = maxminddb::Reader::open_readfile(
+        "/home/mononofu/tmp/GeoLite2-City_20210706/GeoLite2-City.mmdb",
+    )
+    .unwrap();
+    rocket::build()
+        .manage(reader)
+        .mount("/", routes![index, visit_pixel, visit_beacon, exit_beacon])
 }
